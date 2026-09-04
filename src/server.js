76d03a327450dc91040df94061f098d815bee7b0
pedro00876 <pedrochagas979@gmail.com>
2026-09-04 07:36:58 -0300

    rotas

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  const {method} = req
+  const { method, url } = req;
 
-  return res.writeHead(200).end("method: " + method)
+  if (method === "GET" && url === "/products") {
+    return res.writeHead(200).end("Listagem de produtos");
+  }
+
+  if (method === "POST" && url === "/products") {
+    return res.writeHead(201).end("Criação de produto");
+  }
+
+  return res.writeHead(404).end("Not Found");
 });
 
 
